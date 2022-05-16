486d73a11d5aa9b91160d5dc456641245c8ce6a6
mfloreszarate <74306486+mfloreszarate@users.noreply.github.com>
2022-05-16 13:17:19 -0300

    integracion typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,42 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { ProductModule } from './products/product.module';
 
 @Module({
   imports: [
     ProductModule,
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    // }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get("DB_HOST"),
+        port: +configService.get("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASS"),
+        database: configService.get("DB_DATABASE"),
+        entities: [join(__dirname, "**/**.entity{.ts,.js}")],
+        synchronize: configService.get<boolean>("BD_SYNCHRONIZE"),
+        logging: configService.get<boolean>("DB_LOGGING"),
+        // namingStrategy: new SnakeNamingStrategy(),
+        // extra: {
+        //   charset: configService.get("DB_CHARSET")
+        // }
+      }),
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
 
-  ],
-  controllers: [],
-  providers: [],
+  ]
 })
 export class AppModule { }
